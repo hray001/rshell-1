c401454579b1b502551f62436259b1116c5507ec
jstod001 <jstod001@ucr.edu>
2014-10-19 16:03:08 -0700

    added user input functionality

--- src/exec.cpp
+++ src/exec.cpp
@@ -2,10 +2,12 @@
 // exec.cpp for rshell
 
 #include <iostream>
+#include <cstring>
 #include <string>
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -22,14 +24,31 @@ int main()
     {
         cout << "child process" << endl;
         
-        string input;
+        string str_input;
+        cout << "$ ";           //serves as command prompt
+        getline(cin, str_input);
 
-        cout << "$ ";
-        getline(cin, input);
-        input.c_str();      //converts string into a cstring
+        cout << "Original string: " << str_input << endl << endl;
+
+        char* c_input = new char[str_input.length() + 1];
+        strcpy(c_input, str_input.c_str());
+
+        cout << "Converted to cstring: " << c_input << endl << endl;
+        
+        /*
+        char **token;
+        token = strtok(c_input, " ");
+        while(token != NULL)
+        {
+            token = strtok(NULL, " ");
+        }
+        */
 
         // testing
-        cout << "Test string: " << input << endl << endl;
+        // cout << "Test c_string: " << c_input << endl << endl;
+
+        // new cstring now has to be tokenized and placed into a char**
+        // for the exec vp function
 
         // execvp(argv[0], argv);
 

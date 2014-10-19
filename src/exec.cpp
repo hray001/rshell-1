af560f3470bb3b38420e9cb079d1d2a38b55726c
jstod001 <jstod001@ucr.edu>
2014-10-18 20:54:15 -0700

    initial testing and experimentation of program in exec.cpp

--- src/exec.cpp
+++ src/exec.cpp
@@ -1,18 +1,35 @@
-// Author: Jan Kristian Sto. Domingo [jstod001@ucr.edu]
+// Maintainer: Jan Kristian Sto. Domingo [jstod001@ucr.edu]
 // exec.cpp for rshell
 
 #include <iostream>
 #include <string>
+#include <errno.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 using namespace std;
 
-int main(int argc, char** argv)
+//Notes on function formats:
+//execvp(const char *file, char *const argv[])
+
+int main(int argc, char* argv[])
 {
-    
+    int PID = fork();
+    if(PID == 0)
+    {
+        cout << "child process" << endl;
+        
+        execvp("echo", argv);
+
+    }
+    else
+    {
+        wait(0);
+        cout << "parent process" << endl;
+    }
 
     return 0;
 }

99a64e54f34f86189f5f0e526ee5de124b501499
jstod001 <jstod001@ucr.edu>
2014-10-19 20:03:26 -0700

    basic functionality added in exec.cpp

--- src/exec.cpp
+++ src/exec.cpp
@@ -17,46 +17,80 @@ using namespace std;
 //Notes on function formats:
 //execvp(const char *file, char *const argv[])
 
+// tokenCounter function used to determine size of dynamically
+// allocated array for use in the execvp function
+unsigned tokenCounter(char* a)
+{
+    unsigned count = 0;
+    char* token;
+
+    token = strtok(a, " ");
+
+    while(token != NULL)
+    {
+        token = strtok(NULL, " ");
+        count++;
+    }
+
+    return count;
+}
+
 int main()
 {
     int PID = fork();
     if(PID == 0)
     {
-        cout << "child process" << endl;
+        //cout << "child process" << endl;
         
         string str_input;
         cout << "$ ";           //serves as command prompt
         getline(cin, str_input);
 
-        cout << "Original string: " << str_input << endl << endl;
+        // output message for test purposes
+        // cout << "Original string: " << str_input << endl << endl;
 
         char* c_input = new char[str_input.length() + 1];
         strcpy(c_input, str_input.c_str());
 
-        cout << "Converted to cstring: " << c_input << endl << endl;
+        // output message for test purposes
+        // cout << "Converted to cstring: " << c_input << endl << endl;
         
-        /*
-        char **token;
-        token = strtok(c_input, " ");
-        while(token != NULL)
-        {
-            token = strtok(NULL, " ");
-        }
-        */
-
-        // testing
-        // cout << "Test c_string: " << c_input << endl << endl;
-
         // new cstring now has to be tokenized and placed into a char**
         // for the exec vp function
 
-        // execvp(argv[0], argv);
+        char* tmp = new char[str_input.length() + 1];
+        strcpy(tmp, c_input);
+        // tmp protects the original cstring input
+        // tmp will be used for the tokenCounter function
 
+        unsigned token_count = 0;
+        token_count = tokenCounter(tmp);
+
+        char** arg;
+        arg = new char* [token_count];
+        
+        // output message for test purposes
+        // cout << "c_input after tokenCounter: " << c_input << endl;
+
+        // the following chunk of code will take each token and place it into arg
+        // which will then be used as a parameter in execvp
+        unsigned i = 0;
+        char* ptr;
+        ptr = strtok(c_input, " ");
+        while(ptr != NULL)
+        {
+            arg[i] = ptr;
+            ptr = strtok(NULL, " ");
+            i++;
+        }
+
+        //cout << arg[0] << endl;
+        execvp(arg[0], arg);
     }
     else
     {
         wait(0);
-        cout << "parent process" << endl;
+        //cout << "parent process" << endl;
     }
 
     return 0;

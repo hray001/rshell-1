16cf74802022c0c68f633303ae81ee0e11fbca1b
jstod001 <jstod001@ucr.edu>
2014-10-21 15:58:33 -0700

    edited exec.cpp

--- src/exec.cpp
+++ src/exec.cpp
@@ -11,10 +11,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-//#include <boost/tokenizer.hpp>
+#include <boost/tokenizer.hpp>
 
 using namespace std;
-//using namespace boost;
+using namespace boost;
 
 //Notes on function formats:
 //execvp(const char *file, char *const argv[])
@@ -26,11 +26,11 @@ unsigned tokenCounter(char* a)
     unsigned count = 0;
     char* token;
 
-    token = strtok(a, " ;&|#");
+    token = strtok(a, " ");
 
     while(token != NULL)
     {
-        token = strtok(NULL, " ;&|#");
+        token = strtok(NULL, " ");
         count++;
     }
 
@@ -55,10 +55,10 @@ int main()
         // output message for test purposes
         // cout << "Original string: " << str_input << endl << endl;
 
-        
+        /*
         char* c_input = new char[str_input.length() + 1];
         strcpy(c_input, str_input.c_str());
-        
+        */
 
         // output message for test purposes
         // cout << "Converted to cstring: " << c_input << endl << endl;
@@ -66,19 +66,22 @@ int main()
         // new cstring now has to be tokenized and placed into a char**
         // for the exec vp function
 
-        
+        /*
         char* tmp = new char[str_input.length() + 1];
         strcpy(tmp, c_input);
-        
+        */
+
         // tmp protects the original cstring input
         // tmp will be used for the tokenCounter function
 
+        /*
         unsigned token_count = 0;
         token_count = tokenCounter(tmp);
-
+        
         char** arg;
         arg = new char* [token_count + 1];
-        
+        */
+
         // output message for test purposes
         // cout << "c_input after tokenCounter: " << c_input << endl;
 
@@ -86,45 +89,55 @@ int main()
         // the following chunk of code will take each token
         // and place it into arg which will then be used as
         // a parameter in execvp
+        /*
         unsigned i = 0;
         char* ptr;
-        ptr = strtok(c_input, " ;&#|");
+        ptr = strtok(c_input, " ");
         while(ptr != NULL)
         {
             arg[i] = ptr;
             // cout message for test purposes
             cout << "arg[" << i << "] = " << arg[i] << endl;
             // cout << "arg[" << i << "] length = " << strlen(arg[i]) << endl;
-            ptr = strtok(NULL, " ;&#|");
+            ptr = strtok(NULL, " ");
             i++;
         }
         arg[i] = '\0';
-        
-
-        /*
-        char_separator<char> separ(" ;&|");
-        tokenizer<char_separator<char> > token_list(str_input, separ);
         */
 
+        
+        char_separator<char> separ(" ;&|");
+        tokenizer<char_separator<char> > token_list(str_input, separ);
+        
+        /*
         //cout << arg[0] << endl;
-        if(string(arg[0]) == "exit")
+        //if(string(arg[0]) == "exit")
+        if(token_list(0) == "exit")
         {
             //cout message for test purposes
             cout << "\t<executing exit(0)>" << endl;
             exit(0);
         }
+        */
 
         int PID = fork();
         if(PID == 0) 
         {
             //cout message for test purposes
-            cout << "\t<in execvp>" << endl;
-            int exec = execvp(arg[0], arg);
-            perror(arg[0]);
+            //cout << "\t<in execvp>" << endl;
+
+            //int exec = execvp(arg[0], arg);
+            /*
+            int exec = execvp(token_list(0), token_list)
+            perror(token_list(0));
             if(exec == -1)
             {
                 exit(0);
             }
+            */
+            
+            
+
         }
         else
         {
